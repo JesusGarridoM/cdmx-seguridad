33517764da097a55cd144a94203f249961a3b371
JesusGarridoM <j.garridomtz@gmail.com>
2024-03-12 15:47:26 -0600

    add cologias.geojson

--- downloader.py
+++ downloader.py
@@ -3,7 +3,7 @@
 
 import requests
 
-def download_dataset(url, file_name):
+def download_file(url, file_name):
     # Download CSV
     print("Connecting to datos.cdmx.gob.mx...")
     response = requests.get(url, stream=True)
@@ -22,11 +22,20 @@ def download_dataset(url, file_name):
     else:
         raise RuntimeError(f"Error: {response.status_code}")
 
-# Ejemplo de uso
+# Descargar archivo de victimas
 url = "https://archivo.datos.cdmx.gob.mx/FGJ/victimas/victimasFGJ_acumulado_2024_01.csv"
 file_name = "/data/victimasFGJ_acumulado_2024_01.csv"
 
 try:
-    download_dataset(url, file_name)
+    download_file(url, file_name)
+except RuntimeError as error:
+    print(error)
+
+# Descargar catalogo de colonias
+url = "https://datos.cdmx.gob.mx/dataset/02c6ce99-dbd8-47d8-aee1-ae885a12bb2f/resource/026b42d3-a609-44c7-a83d-22b2150caffc/download/catlogo-de-colonias.json"
+file_name = "/data/catlogo-de-colonias.geojson"
+
+try:
+    download_file(url, file_name)
 except RuntimeError as error:
     print(error)
