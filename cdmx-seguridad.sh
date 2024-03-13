3876794c5967102ae96ae0473cb3fe0b3ba564bf
JesusGarridoM <j.garridomtz@gmail.com>
2024-03-12 21:08:27 -0600

    add cdmx-colonias automation

--- cdmx-seguridad.sh
+++ cdmx-seguridad.sh
@@ -2,36 +2,44 @@
 
 # Script para levantar ambiente de manera automatica
 
-echo "Levantando ambiente elk-sandbox..."
+echo 'Levantando ambiente elk-sandbox...'
 cd ../elk-sandbox
 docker compose up -d &
 wait $!
 if [ $? -eq 0 ]
   then
-    cd ../cdmx-seguridad
-    echo Levantando cdmx-seguridad...
-    docker compose up -d &
+    sudo docker cp elk-sandbox-es01-1:/usr/share/elasticsearch/config/certs/ca/ certs/
     wait $!
     if [ $? -eq 0 ]
       then
-        echo "Modificando archivos de filebeat..."
-        sudo chown root:root filebeat.yml 
-        sudo chmod go-w filebeat.yml 
-        echo "Exportando configuracion a logstash..."
-        cp logstash/cdmx-seguridad.conf ../elk-sandbox/logstash/pipeline/
-        cp logstash/cdmx-seguridad.json ../elk-sandbox/logstash/templates/
-        cat logstash/pipelines.yml > ../elk-sandbox/logstash/config/pipelines.yml
-        echo "Reiniciando logstash..."
-        docker restart elk-sandbox-logstash01-1 &
+        cd ../cdmx-seguridad
+        echo 'Modificando archivos de filebeat...'
+        sudo chown root:root filebeat/*
+        sudo chmod go-w filebeat/*
+        echo Levantando cdmx-seguridad...
+        docker compose up -d &
         wait $!
         if [ $? -eq 0 ]
           then
-            echo "Listo!"
+            echo 'Exportando configuracion a logstash...'
+            cp logstash/cdmx-seguridad.conf ../elk-sandbox/logstash/pipeline/
+            cp logstash/cdmx-seguridad.json ../elk-sandbox/logstash/templates/
+            cat logstash/pipelines.yml > ../elk-sandbox/logstash/config/pipelines.yml
+            echo 'Reiniciando logstash...'
+            docker restart elk-sandbox-logstash01-1 &
+            wait $!
+            if [ $? -eq 0 ]
+              then
+                echo 'Listo!'
+              else
+                echo 'Hubo un problema al reiniciar logstash'
+            fi
           else
-            echo "Hubo un problema al reiniciar logstash"
+            echo 'Ocurrio un problema al levantar el ambiente cdmx-seguridad'
+        fi
       else
-        echo "Ocurrio un problema al levantar el ambiente cdmx-seguridad"
+        echo 'Ocurrio un problema al copiar los certificados'
     fi
   else
-    echo "Ocurrio un problema al levantar el ambiente elk-sandbox"
+    echo 'Ocurrio un problema al levantar el ambiente elk-sandbox'
 fi
